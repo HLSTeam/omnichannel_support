01258e4cfae8ebcbcb027d7492000b2d4d4fdf33
Dai Nguyen <dai@devnguyen.com>
2025-10-06 11:13:34 +0700

    remove cors * syntax

--- src/unified-inbox-backend/src/app.js
+++ src/unified-inbox-backend/src/app.js
@@ -31,7 +31,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+//app.options('*', cors());
 app.use(express.json());
 
 // Routes
