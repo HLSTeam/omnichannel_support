67874d4c6fc08d8c93b7ce1021c7f1d393cb9337
Dai Nguyen <dai@devnguyen.com>
2025-10-06 10:17:26 +0700

    update api domain

--- src/unified-inbox-frontend/src/api.js
+++ src/unified-inbox-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://api-support.ninetechvn.com/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
